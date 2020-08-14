181841e854dce43ee7047ee902c5bd33aea708bd
陆朝维 <luchaowei@goodsogood.com>
2020-08-14 08:36:51 +0800

    pro-server update

--- app/prod-server.js
+++ app/prod-server.js
@@ -2,29 +2,8 @@ const Koa = require('koa');
 const path = require('path');
 const router = require('./router');
 const static = require('koa-static');
-// const webpack = require('webpack');
-// const koaWebpack = require('koa-webpack');
-// const config = require('../webpack/webpack.config.client.js');
-// const compiler = webpack(config);
 const app = new Koa();
-// const html = require('../build/server/main');
 const start = async () => {
-    // const middleware = await koaWebpack({
-    //     compiler,
-    //     devMiddleware: {
-    //       methods: ['GET', 'POST'],
-    //       headers: {'X-Custom-Header': 'yes'},
-    //       publicPath: '/',
-    //     },
-
-    //     hotClient: {
-    //       allEntries: false,
-    //       autoConfigure: false,
-    //       logTime: false,
-    //       logLevel: 'error',
-    //     },
-    // });
-    // app.use(middleware);
     app.use(static(path.resolve(__dirname, '../build')));
     app.use(router.middleware());
 
