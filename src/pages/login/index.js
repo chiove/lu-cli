b1f3c1e29cf08a661bf01d271f27f9a62769b7b5
陆朝维 <luchaowei@goodsogood.com>
2020-09-10 15:07:21 +0800

    重置样式

---
+++ src/pages/login/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import getInitialProps from 'src/utils/get-initial-props';
+import {Form, Input, Button, Checkbox} from 'antd';
+import {UserOutlined, LockOutlined} from '@ant-design/icons';
+import './style.less';
+
+const NormalLoginForm = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+  return (
+    <div className="login">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default getInitialProps(NormalLoginForm);
