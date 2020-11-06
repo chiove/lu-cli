867f8203faf35649a748601cef5618ca505f52c9
陆朝维 <luchaowei@goodsogood.com>
2020-11-06 17:15:41 +0800

    优化

--- build/server/app.js
+++ build/server/app.js
@@ -445,7 +445,7 @@ var A = function A(props) {
       height: 100,
       width: 100
     }
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("div", null, "11555"));
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement("div", null, "88889"));
 };
 
 A.getInitialProps = /*#__PURE__*/function () {
--- src/pages/a.js
+++ src/pages/a.js
@@ -24,7 +24,7 @@ const A = props => (<div className="test">
       width: 100,
     }}
   />
-  <div>11555</div>
+  <div>88889</div>
 </div>);
 A.getInitialProps = async (ctx) => {
   if (__CLIENT__) {
