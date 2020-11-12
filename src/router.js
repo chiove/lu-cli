f2ec45f4290c0ae078968d17c7f8d83a7399103c
陆朝维 <luchaowei@goodsogood.com>
2020-11-12 14:53:07 +0800

    按需加载优化

--- src/router.js
+++ src/router.js
@@ -1,20 +1,20 @@
-import loadble from './utils/loadble';
+import lazy from './utils/lazy';
 
 const routes = [
   {
     path: '/home',
     exact: true,
-    component: loadble(() => import(/* webpackChunkName: 'index' */'./pages/index')),
+    component: lazy(() => import(/* webpackChunkName: 'index' */'./pages/index')),
     routes: [
       {
         path: ['/home', '/home/a'],
         exact: true,
-        component: loadble(() => import(/* webpackChunkName: 'a' */'./pages/a')),
+        component: lazy(() => import(/* webpackChunkName: 'a' */'./pages/a')),
       },
       {
         path: '/home/login1',
         exact: true,
-        component: loadble(() => import(/* webpackChunkName: 'login' */'./pages/login')),
+        component: lazy(() => import(/* webpackChunkName: 'login' */'./pages/login')),
       },
     ],
   },
@@ -22,31 +22,31 @@ const routes = [
     path: '/login',
     exact: true,
     ssr: true,
-    component: loadble(() => import(/* webpackChunkName: 'login' */'./pages/login')),
+    component: lazy(() => import(/* webpackChunkName: 'login' */'./pages/login')),
   },
   {
     path: '/a',
     ssr: true,
-    component: loadble(() => import(/* webpackChunkName: 'a' */'./pages/a')),
+    component: lazy(() => import(/* webpackChunkName: 'a' */'./pages/a')),
   },
   {
     path: '/b',
     ssr: true,
-    component: loadble(() => import(/* webpackChunkName: 'b' */'./pages/b')),
+    component: lazy(() => import(/* webpackChunkName: 'b' */'./pages/b')),
   },
   {
     path: '/c',
     ssr: true,
-    component: loadble(() => import(/* webpackChunkName: 'c' */'./pages/c')),
+    component: lazy(() => import(/* webpackChunkName: 'c' */'./pages/c')),
   },
   {
     path: '/test',
     ssr: true,
-    component: loadble(() => import(/* webpackChunkName: 'test' */'./pages/test')),
+    component: lazy(() => import(/* webpackChunkName: 'test' */'./pages/test')),
   },
   {
     path: '*',
-    component: loadble(() => import(/* webpackChunkName: 'not-found' */'./pages/not-found')),
+    component: lazy(() => import(/* webpackChunkName: 'not-found' */'./pages/not-found')),
   },
 ];
 
